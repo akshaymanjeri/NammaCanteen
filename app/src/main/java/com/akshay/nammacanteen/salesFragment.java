415d8230549e8ca489b19250fee591ba0585bb52
Akshay Manjeri <amanjeri627@gmail.com>
2017-11-08 21:52:38 +0530

    Initial commit

--- app/src/main/java/com/akshay/nammacanteen/salesFragment.java
+++ app/src/main/java/com/akshay/nammacanteen/salesFragment.java
@@ -36,6 +36,7 @@ public class salesFragment extends Fragment {
     private static final String url="Enter your Sales List URL";
     List<Sales> salesList;
     RecyclerView recyclerView;
+    
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
