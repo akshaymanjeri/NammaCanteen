ed27db296287b7df9bbf9232d29062ec58cb5488
Akshay Manjeri <amanjeri627@gmail.com>
2017-11-08 21:56:28 +0530

    PHP file

---
+++ Canteen_Delete.php
@@ -0,0 +1,13 @@
+<?php
+$mysql_host='localhost';
+$mysql_user='id3044780_admin';
+$mysql_password='admin';
+$mysql_dbname='id3044780_canteen';
+$conn=mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_dbname);
+$cid=$_POST["cid"];
+$stmt="DELETE FROM `category` WHERE cid like '$cid';";
+$result=mysqli_query($conn ,$stmt);
+$stmt1="DELETE FROM `item` WHERE cid like '$cid';";
+$result1=mysqli_query($conn ,$stmt1);
+echo "Delete Successful";
+?>
