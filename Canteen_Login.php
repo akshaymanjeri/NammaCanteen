e605fdf0df970be9af974eac39b0e5ca54d33aec
Akshay Manjeri <amanjeri627@gmail.com>
2017-11-08 22:02:43 +0530

    PHP file

---
+++ Canteen_Login.php
@@ -0,0 +1,21 @@
+<?php
+$mysql_host='localhost';
+$mysql_user='id3044780_admin';
+$mysql_password='admin';
+$mysql_dbname='id3044780_canteen';
+
+$conn=mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_dbname);
+$username=$_POST["username"];
+$pass=$_POST["password"];
+$role='User';
+$mysql_qry="SELECT * FROM users WHERE phoneno like '$username' and pswd like '$pass' and role like '$role';";
+$result=mysqli_query($conn ,$mysql_qry);
+if(mysqli_num_rows($result) > 0) {
+echo "Login successful";
+$mysql_qry2="INSERT INTO phndummy VALUES ('$username');";
+$result2=mysqli_query($conn,$mysql_qry2);
+}
+else {
+echo "Login unsuccessful";
+}
+?>
